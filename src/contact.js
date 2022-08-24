462ad1c326e2c2b0b90490757fc7997f9245ba85
ritizzzz <kandelritiz@gmail.com>
2022-08-24 23:02:17 +1000

    location added

--- src/contact.js
+++ src/contact.js
@@ -10,9 +10,35 @@ function populateContact(){
     contactContainer.classList.add('contactContainer');
     bgContact.appendChild(contactContainer);
 
-    content.appendChild(bgContact);
-    
+    const contactHeader = document.createElement('h1');
+    contactHeader.innerText = "CONTACT US";
+    contactHeader.classList.add('contactHeader');
+    contactContainer.appendChild(contactHeader);
 
+    const locationDiv = document.createElement('div');
+    locationDiv.classList.add('locationDiv');
+    
+    const image = document.createElement('div');
+    image.classList.add('locationImage');
+    locationDiv.appendChild(image);
+
+    const locationTitle = document.createElement('h3');
+    locationTitle.classList.add('locationTitle');
+    locationTitle.innerText = "Location";
+    locationDiv.appendChild(locationTitle);
+
+    const locationAddress = document.createElement('h3');
+    locationAddress.classList.add('locationAddress');
+    locationAddress.innerText = "333 forest road, Japan";
+    locationDiv.appendChild(locationAddress);
+    
+    const locationText = document.createElement('h3');
+    locationText.classList.add('locationText');
+    locationText.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt";
+    locationDiv.appendChild(locationText);
+    
+    contactContainer.appendChild(locationDiv);
+    content.appendChild(bgContact);
 }
 
 export default populateContact;
