9120c46c2b13e6bdb48bc758f29c030cbcf08c29
ritizzzz <kandelritiz@gmail.com>
2022-08-25 17:51:39 +1000

    manager tile finished

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,6 @@
 import "./contact.css";
+import phone from "./phone.svg";
+import email from "./email.svg";
 function populateContact(){
     document.querySelector('.contact').classList.add('active');
     const content = document.querySelector('#content');
@@ -37,7 +39,53 @@ function populateContact(){
     locationText.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt";
     locationDiv.appendChild(locationText);
     
+    const managerDiv = document.createElement('div');
+    managerDiv.classList.add('managerDiv');
+
+    const managerImage = document.createElement('div');
+    managerImage.classList.add('managerImage');
+    managerDiv.appendChild(managerImage);
+
+    const managerTitle = document.createElement('h3');
+    managerTitle.classList.add('managerTitle');
+    managerTitle.innerText = 'Manager';
+    managerDiv.appendChild(managerTitle);
+
+    const managerContactDiv = document.createElement('div');
+
+    const phoneDiv = document.createElement('div');
+    phoneDiv.classList.add('phoneDiv');
+
+    const phoneIcon = document.createElement('img');
+    phoneIcon.classList.add('phoneIcon');
+    phoneIcon.setAttribute('src', phone);
+    phoneDiv.appendChild(phoneIcon);
+
+    const phoneNum = document.createElement('p');
+    phoneNum.classList.add('phoneNum');
+    phoneNum.innerText = '999-999-999';
+    phoneDiv.appendChild(phoneNum);
+
+    const emailDiv = document.createElement('div');
+    emailDiv.classList.add('emailDiv');
+
+    const emailIcon = document.createElement('img');
+    emailIcon.classList.add('emailIcon');
+    emailIcon.setAttribute('src', email);
+    emailDiv.appendChild(emailIcon);
+
+    const managerEmail = document.createElement('p');
+    managerEmail.classList.add('email');
+    managerEmail.innerText = 'managershiba@somedomain.com';
+    emailDiv.appendChild(managerEmail);
+
+    managerContactDiv.appendChild(phoneDiv);
+    managerContactDiv.appendChild(emailDiv);
+ 
+    managerDiv.appendChild(managerContactDiv);
+    
     contactContainer.appendChild(locationDiv);
+    contactContainer.appendChild(managerDiv);
     content.appendChild(bgContact);
 }
 
